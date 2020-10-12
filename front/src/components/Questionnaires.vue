759b30b26162cbe80372b8465b7d9543b5072c7b
teploff <teploff.a@mail.ru>
2020-10-12 20:12:19 +0300

    dynamic loading cards

--- front/src/components/Questionnaires.vue
+++ front/src/components/Questionnaires.vue
@@ -1,47 +1,15 @@
 <template>
   <div class="card-expansion">
-    <md-card>
+    <md-card v-for="card in cards.questionnaires" v-bind:key="card.email">
       <md-card-media>
-        <img src="static/images/boy.png" alt="People">
+        <img v-if="card.sex === 'male'" src="static/images/boy.png" alt="People">
+        <img v-else src="static/images/girl.png" alt="People">
       </md-card-media>
 
       <md-card-header>
-        <div class="md-title">Title goes here</div>
-        <div class="md-subhead">Subtitle here</div>
-      </md-card-header>
-
-      <md-card-expand>
-        <md-card-actions md-alignment="space-between">
-          <div>
-            <md-button>Action</md-button>
-            <md-button>Action</md-button>
-          </div>
-
-          <md-card-expand-trigger>
-            <md-button class="md-icon-button">
-              <md-icon>keyboard_arrow_down</md-icon>
-            </md-button>
-          </md-card-expand-trigger>
-        </md-card-actions>
-
-        <md-card-expand-content>
-          <md-card-content>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio.
-            Dolores, sed accusantium quasi non, voluptas eius illo quas,
-            saepe voluptate pariatur in deleniti minus sint. Excepturi.
-          </md-card-content>
-        </md-card-expand-content>
-      </md-card-expand>
-    </md-card>
-
-    <md-card>
-      <md-card-media>
-        <img src="static/images/girl.png" alt="People">
-      </md-card-media>
-
-      <md-card-header>
-        <div class="md-title">Title goes here</div>
-        <div class="md-subhead">Subtitle here</div>
+        <div class="md-title">{{ card.name }} {{ card.surname }}</div>
+        <div class="md-subhead">{{ card.email }}</div>
+        <div class="md-subhead">{{ card.sex }}</div>
       </md-card-header>
 
       <md-card-expand>
@@ -53,9 +21,9 @@
 
         <md-card-expand-content>
           <md-card-content>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio.
-            Dolores, sed accusantium quasi non, voluptas eius illo quas,
-            saepe voluptate pariatur in deleniti minus sint. Excepturi.
+            <p> Birthday: {{ card.birthday }} </p>
+            <p> City: {{ card.city }} </p>
+            <p> Interests: {{ card.interests }} </p>
           </md-card-content>
         </md-card-expand-content>
       </md-card-expand>
@@ -74,7 +42,10 @@ export default {
       limit: 10,
       offset: 0,
     },
-    questionnaires: null,
+    cards: {
+      questionnaires: null,
+      count: 0,
+    },
   }),
   methods: {
     getQuestionnaires() {
@@ -82,7 +53,7 @@ export default {
       headers.Authorization = localStorage.getItem('access_token');
       axios.post(path, this.payload, { headers })
         .then((response) => {
-          this.questionnaires = JSON.parse(JSON.stringify(response.data));
+          this.cards = JSON.parse(JSON.stringify(response.data));
         })
         .catch((error) => {
           const err = JSON.parse(JSON.stringify(error.response));
