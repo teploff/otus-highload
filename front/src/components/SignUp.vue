44ef7bba389e968a4a8b62cdf07e73d4b3bae25d
teploff <teploff.a@mail.ru>
2020-10-12 15:31:11 +0300

    sign up: delete extra classes

--- front/src/components/SignUp.vue
+++ front/src/components/SignUp.vue
@@ -7,54 +7,54 @@
             <h2 class="form-title">Sign up</h2>
 
             <form>
-              <md-field class="form-group">
+              <md-field>
                 <md-icon>email</md-icon>
                 <label>Your Email</label>
                 <md-input v-model="payload.email" type="email" required></md-input>
               </md-field>
 
-              <md-field class="form-group">
+              <md-field>
                 <md-icon>lock</md-icon>
                 <label>Your Password</label>
                 <md-input v-model="payload.password" type="password" required></md-input>
               </md-field>
 
-              <md-field class="form-group" :md-toggle-password="false">
+              <md-field :md-toggle-password="false">
                 <md-icon>lock_open</md-icon>
                 <label>Type your password again</label>
                 <md-input type="password" required></md-input>
               </md-field>
 
-              <md-field class="form-group">
+              <md-field>
                 <md-icon>person</md-icon>
                 <label>Your Name</label>
                 <md-input v-model="payload.name" type="text" required></md-input>
               </md-field>
 
-              <md-field class="form-group">
+              <md-field>
                 <md-icon>person</md-icon>
                 <label>Your Surname</label>
                 <md-input v-model="payload.surname" type="text" required></md-input>
               </md-field>
 
-              <div class="form-group">
+              <div>
                 <md-datepicker v-model="payload.birthday" required>
                   <label>Your Birthday</label>
                 </md-datepicker>
               </div>
 
-              <div class="form-group" align="center">
+              <div align="center">
                 <md-radio v-model="payload.sex" value="male">Male</md-radio>
                 <md-radio v-model="payload.sex" value="female">Female</md-radio>
               </div>
 
-              <md-field class="form-group">
+              <md-field>
                 <md-icon>location_city</md-icon>
                 <label>Your City</label>
                 <md-input v-model="payload.city" type="text" required></md-input>
               </md-field>
 
-              <div class="form-group">
+              <div>
                 <md-icon>flight_takeoff</md-icon>
                 <md-field>
                   <label>Your Interests</label>
@@ -62,7 +62,7 @@
                 </md-field>
               </div>
 
-              <div class="form-group form-button">
+              <div class="form-button">
                 <md-button
                   class="md-dense md-raised md-primary sign-up-button"
                   id="signUpButton"
@@ -172,10 +172,6 @@ export default {
     margin-left: 75px;
     margin-right: 75px;
     padding-left: 34px;
-  }
-
-  .form-group:last-child {
-    margin-bottom: 0;
   }
 
   .signup-image-link {
