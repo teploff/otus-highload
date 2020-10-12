2ae27e25fde52b8dfcb66a0a9e40b6dd9e5dff97
teploff <teploff.a@mail.ru>
2020-10-12 12:00:58 +0300

    make layout

--- front/src/components/SignUp.vue
+++ front/src/components/SignUp.vue
@@ -1,60 +1,102 @@
 <template>
-  <form>
-    <md-field>
-      <md-icon>email</md-icon>
-      <label>Your Email</label>
-      <md-input v-model="payload.email" type="email" required></md-input>
-    </md-field>
+  <div class="main">
+    <section class="sign-up">
+      <div class="container">
+        <div class="sign-up-content">
+          <div class="md-layout-item md-size-337">
+            <h2 class="form-title">Sign up</h2>
+            <div class="sign-up-form">
+              <form class="register-form">
+                <md-field class="form-group">
+                  <md-icon>email</md-icon>
+                  <label>Your Email</label>
+                  <md-input v-model="payload.email" type="email" required></md-input>
+                </md-field>
 
-    <md-field>
-      <md-icon>lock</md-icon>
-      <label>Your Password</label>
-      <md-input v-model="payload.password" type="password" required></md-input>
-    </md-field>
+                <md-field class="form-group">
+                  <md-icon>lock</md-icon>
+                  <label>Your Password</label>
+                  <md-input v-model="payload.password" type="password" required></md-input>
+                </md-field>
 
-    <md-field :md-toggle-password="false">
-      <md-icon>lock_open</md-icon>
-      <label>Type your password again</label>
-      <md-input type="password" required></md-input>
-    </md-field>
+                <md-field class="form-group" :md-toggle-password="false">
+                  <md-icon>lock_open</md-icon>
+                  <label>Type your password again</label>
+                  <md-input type="password" required></md-input>
+                </md-field>
 
-    <md-field>
-      <md-icon>person</md-icon>
-      <label>Your Name</label>
-      <md-input v-model="payload.name" type="text" required></md-input>
-    </md-field>
+                <md-field class="form-group">
+                  <md-icon>person</md-icon>
+                  <label>Your Name</label>
+                  <md-input v-model="payload.name" type="text" required></md-input>
+                </md-field>
 
-    <md-field>
-      <md-icon>person</md-icon>
-      <label>Your Surname</label>
-      <md-input v-model="payload.surname" type="text" required></md-input>
-    </md-field>
+                <md-field class="form-group">
+                  <md-icon>person</md-icon>
+                  <label>Your Surname</label>
+                  <md-input v-model="payload.surname" type="text" required></md-input>
+                </md-field>
 
-    <div>
-      <md-datepicker v-model="payload.birthday" required>
-        <label>Your Birthday</label>
-      </md-datepicker>
-    </div>
+                <div class="form-group">
+                  <md-datepicker v-model="payload.birthday" required>
+                    <label>Your Birthday</label>
+                  </md-datepicker>
+                </div>
 
-    <div>
-      <md-radio v-model="payload.sex" value="male">Male</md-radio>
-      <md-radio v-model="payload.sex" value="female">Female</md-radio>
-    </div>
+                <div class="form-group">
+                  <md-radio v-model="payload.sex" value="male">Male</md-radio>
+                  <md-radio v-model="payload.sex" value="female">Female</md-radio>
+                </div>
 
-    <md-field>
-      <md-icon>location_city</md-icon>
-      <label>Your City</label>
-      <md-input v-model="payload.city" type="text" required></md-input>
-    </md-field>
+                <md-field class="form-group">
+                  <md-icon>location_city</md-icon>
+                  <label>Your City</label>
+                  <md-input v-model="payload.city" type="text" required></md-input>
+                </md-field>
 
-    <md-icon>flight_takeoff</md-icon>
-    <md-field>
-      <label>Your Interests</label>
-      <md-textarea v-model="payload.interests" required></md-textarea>
-    </md-field>
-
-    <md-button class="md-raised" id="signUpButton" v-on:click="signUp">Register</md-button>
-  </form>
+                <div class="form-group">
+                  <md-icon>flight_takeoff</md-icon>
+                  <md-field>
+                    <label>Your Interests</label>
+                    <md-textarea v-model="payload.interests" required></md-textarea>
+                  </md-field>
+                </div>
+                <div class="form-group form-button">
+                  <md-button class="md-raised" id="signUpButton" v-on:click="signUp">
+                    Register
+                  </md-button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="md-layout-item md-size-337">
+            <div class="sign-up-image">
+              <table width="100%">
+                <tr>
+                  <td id="signup_image_row_1"></td>
+                </tr>
+                <tr>
+                  <td>
+                    <figure>
+                      <img src="../../static/images/sign_up_image.jpg" alt="sing up image">
+                    </figure>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <a href="#" class="signup-image-link">I am already member</a>
+                  </td>
+                </tr>
+                <tr>
+                  <td id='signup_image_row_4'></td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
