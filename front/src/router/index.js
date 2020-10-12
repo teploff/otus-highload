17b37c48d2bb73e85d705adc84941ce78f93d007
teploff <teploff.a@mail.ru>
2020-10-13 00:46:58 +0300

    dockerize Vue app as production

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Questionnaires from '../components/Questionnaires';
-import SignUp from '../components/SignUp';
-import SignIn from '../components/SignIn';
-import SignUpSuccess from '../components/SignUpSuccess';
+import Questionnaires from '@/components/Questionnaires';
+import SignUp from '@/components/SignUp';
+import SignIn from '@/components/SignIn';
+import SignUpSuccess from '@/components/SignUpSuccess';
 
 Vue.use(Router);
 
