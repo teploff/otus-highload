5a79e05d8d32c72e86813c4c4d035bb4c56d0622
teploff <teploff.a@mail.ru>
2020-11-22 23:48:21 +0300

    server: add constraint for unique key on (id, status, user_id and chat_id) columns

--- server/migrations/4_message.up.sql
+++ server/migrations/4_message.up.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS message (
-   id          VARCHAR(100) NOT NULL DEFAULT (uuid()) PRIMARY KEY,
-   text        TEXT NOT NULL,
-   status      INT NOT NULL UNIQUE CONSTRAINT message_status_field CHECK (status in (0, 1, 2, 3, 4)),
-   create_time TIMESTAMP NOT NULL,
-   user_id     VARCHAR(100) NOT NULL,
-   chat_id     VARCHAR(100) NOT NULL,
-   FOREIGN     KEY(user_id) REFERENCES user(id),
-   FOREIGN     KEY(chat_id) REFERENCES chat(id)
+    id          VARCHAR(100) NOT NULL DEFAULT (uuid()),
+    text        TEXT NOT NULL,
+    status      INT NOT NULL CONSTRAINT message_status_field CHECK (status in (0, 1, 2, 3, 4)),
+    create_time TIMESTAMP NOT NULL,
+    user_id     VARCHAR(100) NOT NULL,
+    chat_id     VARCHAR(100) NOT NULL,
+    CONSTRAINT  uc_id_status_ui_ci UNIQUE(id, status, user_id, chat_id),
+    FOREIGN     KEY(user_id) REFERENCES user(id),
+    FOREIGN     KEY(chat_id) REFERENCES chat(id)
 ) ENGINE=InnoDB;
