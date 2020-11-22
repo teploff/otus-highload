7a233a60544d0576c8ce718e6973468ef044f153
teploff <teploff.a@mail.ru>
2020-11-22 18:49:01 +0300

    server: migrations for messenger

--- server/migrations/1_init.up.sql
+++ server/migrations/1_init.up.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS user (
-    id            VARCHAR(100)  NOT NULL DEFAULT (uuid()) PRIMARY KEY,
+    id            VARCHAR(100) NOT NULL DEFAULT (uuid()) PRIMARY KEY,
     email         VARCHAR(100) NOT NULL UNIQUE CONSTRAINT email_empty_field CHECK (email != ''),
-    password      TEXT         NOT NULL CONSTRAINT password_empty_field CHECK (password != ''),
+    password      TEXT NOT NULL CONSTRAINT password_empty_field CHECK (password != ''),
     name          VARCHAR(100) NOT NULL CONSTRAINT name_empty_field CHECK (name != ''),
     surname       VARCHAR(100) NOT NULL CONSTRAINT surname_empty_field CHECK (surname != ''),
-    sex           VARCHAR(10)  NOT NULL CONSTRAINT sex_empty_field CHECK (sex != '' and (sex = 'male' or sex = 'female')),
-    birthday      DATE         NOT NULL ,
-    city          VARCHAR(50)  NOT NULL CONSTRAINT city_empty_field CHECK (city != ''),
-    interests     TEXT         NOT NULL CONSTRAINT interests_empty_field CHECK (interests != ''),
-    access_token  VARCHAR(350) NULL ,
-    refresh_token VARCHAR(350) NULL ,
-    create_time   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    update_time   TIMESTAMP    NULL
+    sex           VARCHAR(10) NOT NULL CONSTRAINT sex_empty_field CHECK (sex != '' and (sex = 'male' or sex = 'female')),
+    birthday      DATE NOT NULL,
+    city          VARCHAR(50) NOT NULL CONSTRAINT city_empty_field CHECK (city != ''),
+    interests     TEXT NOT NULL CONSTRAINT interests_empty_field CHECK (interests != ''),
+    access_token  VARCHAR(350) NULL,
+    refresh_token VARCHAR(350) NULL,
+    create_time   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_time   TIMESTAMP NULL
 ) ENGINE=InnoDB;
