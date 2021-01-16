5b09d6775943465584ca5135983e290582709ac8
teploff <teploff.a@mail.ru>
2021-01-17 00:01:07 +0300

    frontend: add persistant plugin for vuex

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
@@ -35,5 +36,6 @@ export default new Vuex.Store({
             state.searchPeople.anthroponym = anthroponym
         }
     },
-    actions: {}
+    actions: {},
+    plugins: [createPersistedState()],
 });
