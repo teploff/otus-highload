d06feb2313c777af8661e9bc27027cdea5798559
teploff <teploff.a@mail.ru>
2021-01-25 23:51:52 +0300

    backend: separate two migrations: for mysql and for clickhouse

---
+++ backend/migrations/clickhouse/cluster/0001_init.up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE message (
+    datetime    DateTime,
+    id          UUID,
+    text        String,
+    status      Enum8('created' = 0, 'received' = 1, 'read' = 2, 'deleted' = 3),
+    create_time Int64,
+    user_id     UUID,
+    chat_id     UUID,
+    shard_id    UInt8
+) ENGINE = Distributed(messages, default, message, shard_id);
+
+CREATE TABLE chat (
+    datetime     DateTime,
+    id           UUID,
+    create_time  Int64,
+    participants Array(UUID)
+) ENGINE = MergeTree()
+ORDER BY (datetime, id, create_time, participants);
