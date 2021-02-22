6e47755558dce1dd0b7791eda167494ede764d9d
teploff <teploff.a@mail.ru>
2021-02-22 15:14:05 +0300

    monitoring: messenger: push memory and cpu metrics to zabbix

--- backend/messenger/go.mod
+++ backend/messenger/go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/golang/protobuf v1.4.3
 	github.com/google/go-cmp v0.5.4 // indirect
 	github.com/hashicorp/consul/api v1.3.0
+	github.com/mackerelio/go-osstat v0.1.0
 	github.com/mailru/easyjson v0.0.0-20180823135443-60711f1a8329
 	github.com/opentracing/opentracing-go v1.1.0
 	github.com/pelletier/go-toml v1.4.0 // indirect
--- backend/messenger/internal/infrastructure/zabbix/zabbix.go
+++ backend/messenger/internal/infrastructure/zabbix/zabbix.go
@@ -2,10 +2,14 @@ package zabbix
 
 import (
 	"context"
+	"fmt"
 	"messenger/internal/config"
+	"strconv"
 	"time"
 
 	zbx "github.com/blacked/go-zabbix"
+	"github.com/mackerelio/go-osstat/cpu"
+	"github.com/mackerelio/go-osstat/memory"
 	"go.uber.org/zap"
 )
 
@@ -45,8 +49,68 @@ func (c *Client) Publish(ctx context.Context) {
 func (c *Client) collectMetrics() *zbx.Packet {
 	metrics := make([]*zbx.Metric, 0, 1)
 
-	metrics = append(metrics, zbx.NewMetric(c.hostName, "messenger-cpu", "1.22", time.Now().Unix()))
-	metrics = append(metrics, zbx.NewMetric(c.hostName, "status", "OK", time.Now().Unix()))
+	memMetrics, err := c.collectMemoryMetrics()
+	if err != nil {
+		c.logger.Error("get MEMORY info", zap.Error(err))
+	}
+	metrics = append(metrics, memMetrics...)
+
+	cpuMetrics, err := c.collectCPUMetrics()
+	if err != nil {
+		c.logger.Error("get CPU info", zap.Error(err))
+	}
+	metrics = append(metrics, cpuMetrics...)
 
 	return zbx.NewPacket(metrics)
+}
+
+func (c *Client) collectMemoryMetrics() ([]*zbx.Metric, error) {
+	metrics := make([]*zbx.Metric, 0, 4)
+
+	mem, err := memory.Get()
+	if err != nil {
+		return nil, err
+	}
+
+	metrics = append(metrics, zbx.NewMetric(c.hostName, "msgr-mem-total", strconv.FormatUint(mem.Total, 10),
+		time.Now().Unix()))
+	metrics = append(metrics, zbx.NewMetric(c.hostName, "msgr-mem-used", strconv.FormatUint(mem.Used, 10),
+		time.Now().Unix()))
+	metrics = append(metrics, zbx.NewMetric(c.hostName, "msgr-mem-cached", strconv.FormatUint(mem.Cached, 10),
+		time.Now().Unix()))
+	metrics = append(metrics, zbx.NewMetric(c.hostName, "msgr-mem-free", strconv.FormatUint(mem.Free, 10),
+		time.Now().Unix()))
+
+	return metrics, nil
+}
+
+func (c *Client) collectCPUMetrics() ([]*zbx.Metric, error) {
+	metrics := make([]*zbx.Metric, 0, 4)
+
+	cpuBefore, err := cpu.Get()
+	if err != nil {
+		return nil, err
+		//c.logger.Error("get CPU info", zap.Error(err))
+	}
+
+	time.Sleep(time.Second * 1)
+
+	cpuAfter, err := cpu.Get()
+	if err != nil {
+		return nil, err
+	}
+
+	total := float64(cpuAfter.Total - cpuBefore.Total)
+	cpuUser := float64(cpuAfter.User-cpuBefore.User) / total * 100
+	cpuSystem := float64(cpuAfter.System-cpuBefore.System) / total * 100
+	cpuIdle := float64(cpuAfter.Idle-cpuBefore.Idle) / total * 100
+
+	metrics = append(metrics, zbx.NewMetric(c.hostName, "msgr-cpu-user", fmt.Sprintf("%f", cpuUser),
+		time.Now().Unix()))
+	metrics = append(metrics, zbx.NewMetric(c.hostName, "msgr-cpu-system", fmt.Sprintf("%f", cpuSystem),
+		time.Now().Unix()))
+	metrics = append(metrics, zbx.NewMetric(c.hostName, "msgr-cpu-idle", fmt.Sprintf("%f", cpuIdle),
+		time.Now().Unix()))
+
+	return metrics, nil
 }
